30fb96c09895d70fb03c5e8fa2087e96012225fd
gandhi <ryokuchamugichauroncha.tea5@gmail.com>
2021-01-31 13:40:28 +0900

    Make recommend page responsive

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,23 @@
 (function(){
+
+  const recommendSection = document.querySelector('.recommend');
+  const recommendSectionSp = document.querySelector('.recommend-sp');
+  const renderResponsive = () => {
+    if(screen.width <= 768) {
+      recommendSection.style.display = "none";
+      recommendSectionSp.style.display = "block"
+    } else {
+      recommendSection.style.display = "block";
+      recommendSectionSp.style.display = "none";
+    }
+  }
+
+  renderResponsive();
+
+  window.addEventListener("resize", () => {
+    renderResponsive();
+  })
+
   // Function used to shrink nav bar removing paddings and adding black background
   $(window).scroll(function() {
     if (50 < $(document).scrollTop()) {
