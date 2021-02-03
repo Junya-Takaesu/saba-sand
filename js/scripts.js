a4ae30091d7b15c59569b7d57949f25193ce0b89
gandhi <ryokuchamugichauroncha.tea5@gmail.com>
2021-02-03 21:36:01 +0900

    Add animation to the scroll-top button

--- js/scripts.js
+++ js/scripts.js
@@ -14,4 +14,23 @@
     $("#main-list-div").toggleClass("show_list");
     $("#main-list-div").fadeIn();
   });
+
+  (function(){
+    const scrollTop = $('.scroll-top');
+    let appear = false;
+    window.addEventListener("scroll", () => {
+      if (100 < window.scrollY) {
+        if (appear == false) {
+          appear = true;
+          scrollTop.stop().animate({"right": "30px"}, 300);
+        }
+      } else {
+        if (appear) {
+          appear = false;
+          scrollTop.stop().animate({"right": "-50px"}, 300);
+        }
+      }
+    })
+  })()
+
 })()
