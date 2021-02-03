9cca334acbfc6a40ff5124c02f723a5235a38c94
gandhi <ryokuchamugichauroncha.tea5@gmail.com>
2021-02-03 22:00:20 +0900

    Refactoring

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,36 @@
-(function(){
+(function() {
 
   // Function used to shrink nav bar removing paddings and adding black background
   $(window).scroll(function() {
-    if (50 < $(document).scrollTop()) {
+    if(50 < $(document).scrollTop()) {
         $('.nav').addClass('affix');
     } else {
         $('.nav').removeClass('affix');
     }
   });
 
-  $('.nav-trigger').click(function () {
+  $('.nav-trigger').click(function() {
     $(this).toggleClass('active');
     $("#main-list-div").toggleClass("show_list");
     $("#main-list-div").fadeIn();
   });
 
-  (function(){
+  (function() {
     const scrollTop = $('.scroll-top');
     let appear = false;
-    window.addEventListener("scroll", () => {
-      if (100 < window.scrollY) {
-        if (appear == false) {
+    window.addEventListener("scroll", function() {
+      if(100 < window.scrollY) {
+        if(appear == false) {
           appear = true;
           scrollTop.stop().animate({"right": "30px"}, 300);
         }
       } else {
-        if (appear) {
+        if(appear) {
           appear = false;
           scrollTop.stop().animate({"right": "-50px"}, 300);
         }
       }
     })
-  })()
+  })();
 
-})()
+})();
